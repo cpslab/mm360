792abef9d19b3ef743b07ea8bb39f026188e8d65
nshiba <naoya.shiba310@gmail.com>
2017-09-02 05:28:47 +0900

    Change react-router

--- src/App.js
+++ src/App.js
@@ -1,109 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {
-    BrowserRouter as Router,
     Route,
-    Link
-} from 'react-router-dom'
-import fetch from 'isomorphic-fetch'
-import queryString from 'query-string'
+    Switch
+} from 'react-router-dom';
 
 import Home from './Top.js'
 import Project from "./Project";
 import PanoramicView from "./PanoramicView";
 
-// const policyRootUrl = "http://localhost:5000/api/policy";
-const policyRootUrl = "https://rocky-woodland-39339.herokuapp.com/api/policy";
-
-class Board extends Component {
-    renderSquare(i) {
-        return <Square value={i} />
-    }
-}
-
-class Square extends Component {
-    render() {
-        return (
-            <button className="square">
-                {this.props.value}
-            </button>
-        )
-    }
-}
-
-class Upload extends Component {
-
-    state = { isPosting: false };
-
-    componentDidMount() {
-    }
-
-    handleSubmit = async (e) => {
-        e.preventDefault();
-
-        console.log(this.state.postUrl);
-        this.setState({ isPosting: true });
-
-        await fetch(this.state.postUrl, {
-            method: 'PUT',
-            body: this.state.file,
-            headers: {
-                "Content-Type": this.state.file.type
-            }
-        });
-        this.setState({ isPosting: false })
-    };
-
-    handleFile = (e) => {
-        const reader = new FileReader();
-        const file = e.target.files[0];
-
-        reader.onload = async () => {
-            const param = {
-                project_name: "test",
-                filename: file.name,
-                content_type: file.type
-            };
-
-            const policyUrl = policyRootUrl + "?" + queryString.stringify(param);
-            console.log(policyUrl);
-            const url = await (await fetch(policyUrl)).text();
-
-            this.setState({
-                 postUrl: url,
-                 file: file
-            });
-        };
-
-        reader.readAsDataURL(file);
-    };
-
-    render() {
-
-        return (
-            <div className="App">
-                <form onSubmit={this.handleSubmit} encType="multipart/form-data">
-                    <input type="file" onChange={this.handleFile} />
-                    <input className='btn btn-primary' type="submit" value="Upload" />
-                </form>
-                <p>{this.state.isPosting ? "progress..." : ""}</p>
-            </div>
-
-        )
-    }
-}
-
 const MyRouter = () => (
-    <Router>
-        <div>
-            <Route exact path="/" component={Home}/>
+    <main>
+        <Switch>
+            <Route exact path='/' component={Home} />
             <Route path="/project/:projectName" component={Project}/>
             <Route path="/panorama/:projectName/" component={PanoramicView}/>
-            <Route path="/upload" component={Upload}/>
-        </div>
-    </Router>
+        </Switch>
+    </main>
 );
-
 
 export default MyRouter;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    BrowserRouter,
+    Route,
+    Switch,
+    Link
+} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+), document.getElementById('root'));
 registerServiceWorker();
